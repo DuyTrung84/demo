315200f34132017ee70b6f6ba8b81147e3190ed5
datlqph24221 <lequocdat2312@gmail.com>
2023-05-20 20:43:14 +0700

    Update menu

---
+++ site/inc/menu.php
@@ -0,0 +1,24 @@
+<?php
+    require "../bootstrap/bootstrap.php"
+?>
+<body>
+  <div class="container">
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#home">Trang chủ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#menu1">Điện thoại</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#menu2">Laptop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#menu2">Đồng hồ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#menu2">Máy tính</a>
+      </li>
+    </ul>
+  </div> 
+
